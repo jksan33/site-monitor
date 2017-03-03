f338b8c62749b17d046f8a3c387d7e6889ece362
LiPiZi <lpzimm22@gmail.com>
2017-03-03 09:04:30 -0800

    Update BeautifulSM.py

--- BeautifulSM.py
+++ BeautifulSM.py
@@ -4,18 +4,20 @@ import requests
 
 import time
 
+import difflib
+
 url = raw_input("Enter website to monitor: ")
 
 t = int(raw_input("Wait time: "))
 
 while True:
-	r1  = requests.get("http://" +url)
+	r1  = requests.get(url)
 	data1 = r1.text
 	soup1 = BeautifulSoup(data1)
 
 	time.sleep(t)
 
-	r2  = requests.get("http://" +url)
+	r2  = requests.get(url)
 	data2 = r2.text
 	soup2 = BeautifulSoup(data1)
 
@@ -23,3 +25,6 @@ while True:
 		print("No change")
 	else:
 		print("Change!")
+		d = difflib.Differ()
+		diff = d.compare(soup1, soup2)
+		print '\n'.join(diff)
