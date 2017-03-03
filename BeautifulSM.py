b2c5eec8932fd6940b5700f0a1726481e426ca2a
LiPiZi <lpzimm22@gmail.com>
2017-03-03 09:17:15 -0800

    Update BeautifulSM.py

--- BeautifulSM.py
+++ BeautifulSM.py
@@ -13,13 +13,13 @@ t = int(raw_input("Wait time: "))
 while True:
 	r1  = requests.get(url)
 	data1 = r1.text
-	soup1 = BeautifulSoup(data1)
+	soup1 = BeautifulSoup(data1, "html.parser")
 
 	time.sleep(t)
 
 	r2  = requests.get(url)
 	data2 = r2.text
-	soup2 = BeautifulSoup(data1)
+	soup2 = BeautifulSoup(data2, "html.parser")
 
 	if soup1 == soup2:
 		print("No change")
