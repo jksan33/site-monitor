ec6a0a825e9b47e969326642ddaf394a24925f82
LiPiZi <lpzimm22@gmail.com>
2017-03-07 09:23:45 -0800

    Update BeautifulSM.py

--- BeautifulSM.py
+++ BeautifulSM.py
@@ -6,6 +6,8 @@ import time
 
 import difflib
 
+from pprint import pprint
+
 url = raw_input("Enter website to monitor: ")
 
 t = int(raw_input("Wait time: "))
@@ -24,7 +26,8 @@ while True:
 	if soup1 == soup2:
 		print("No change")
 	else:
-		print("Change!")
-		d = difflib.Differ()
-		diff = d.compare(soup1, soup2)
-		print '\n'.join(diff)
+		l1 = str(soup1).split(' ')
+		l2 = str(soup2).split(' ')
+		d = difflib.Differ();
+		dif = list(d.compare(l1, l2));
+		print(" ".join(['<b>'+i[2:]+'</b>' if i[:1] == '+' else i[2:] for i in dif if not i[:1] in '-?']))
